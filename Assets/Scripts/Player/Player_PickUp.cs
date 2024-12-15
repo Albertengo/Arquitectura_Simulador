0d8e3a0ee889a58984f053790e14471e1d670229
Albertengo <145947798+Albertengo@users.noreply.github.com>
2024-12-15 04:03:18 -0300

    Mecanica de agarrar bugueada

---
+++ Assets/Scripts/Player/Player_PickUp.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player_PickUp : MonoBehaviour
+{
+    [SerializeField]
+    private LayerMask pickable_Layer;
+
+    [SerializeField]
+    private Transform playerCameraTransform;
+
+    [SerializeField]
+    private GameObject pickUpUI;
+
+    [SerializeField]
+    [Min(1)]
+    private float hitRange = 3;
+
+    [SerializeField]
+    private Transform pickUpParent;
+
+    [SerializeField]
+    private GameObject inHandItem;
+
+    [SerializeField]
+    private InputActionReference interactionInput, dropInput, useInput;
+
+    private RaycastHit hit;
+
+    [SerializeField]
+    private AudioSource pickUpSource;
+
+    private void Start()
+    {
+        interactionInput.action.performed += PickUp;
+        dropInput.action.performed += Drop;
+    }
+
+    private void Drop(InputAction.CallbackContext obj)
+    {
+        if (inHandItem != null)
+        {
+            inHandItem.transform.SetParent(null);
+            inHandItem = null;
+            Rigidbody rb = hit.collider.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.isKinematic = false;
+            }
+        }
+    }
+
+    private void PickUp(InputAction.CallbackContext obj)
+    {
+        if(hit.collider != null && inHandItem == null)
+        {
+            IPickable pickableItem = hit.collider.GetComponent<IPickable>();
+            if (pickableItem != null)
+            {
+                //pickUpSource.Play();
+                inHandItem = pickableItem.PickUp();
+                inHandItem.transform.SetParent(pickUpParent.transform, pickableItem.KeepWorldPosition);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        DeactivateHighlight();
+
+        if (inHandItem != null)
+        {
+            return;
+        }
+
+        ActivateHighlight();
+        
+    }
+    #region Code
+    void DeactivateHighlight()
+    {
+        if (hit.collider != null)
+        {
+            hit.collider.GetComponent<Highlight>()?.ToggleHighlight(false);
+            pickUpUI.SetActive(false);
+        }
+    }
+
+    void ActivateHighlight()
+    {
+        if (Physics.Raycast(playerCameraTransform.position, playerCameraTransform.forward, out hit, hitRange, pickable_Layer))
+        {
+            hit.collider.GetComponent<Highlight>()?.ToggleHighlight(true);
+            pickUpUI.SetActive(true);
+        }
+    }
+    #endregion
+}
